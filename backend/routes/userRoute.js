42cfc13d3f3d9d82329fde95c45c4284f824aa6d
FuadAbdullah97 <ifuadabdullah@gmail.com>
2025-01-03 23:30:41 +0600

    Front end setup

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
 import {getUsersForSidebar} from "../controllers/user.controller.js";
+import res from "express/lib/response.js";
 
 const router = express.Router();
 
 
 router.get("/",protectRoute, getUsersForSidebar)
-
+router.get("/", ()=>{
+    res.status(200).json({message : "User Path Test"})
+})
 export default router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,14 +9,23 @@ function App() {
 
   return (
       <>
-
-          <button className="btn">Button</button>
-          <button className="btn btn-neutral">Neutral</button>
-          <button className="btn btn-primary">Primary</button>
-          <button className="btn btn-secondary">Secondary</button>
-          <button className="btn btn-accent">Accent</button>
-          <button className="btn btn-ghost">Ghost</button>
-          <button className="btn btn-link">Link</button>
+          <div
+              className="hero min-h-screen"
+              style={{
+                  backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+              }}>
+              <div className="hero-overlay bg-opacity-60"></div>
+              <div className="hero-content text-neutral-content text-center">
+                  <div className="max-w-md">
+                      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                      <p className="mb-5">
+                          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                          quasi. In deleniti eaque aut repudiandae et a id nisi.
+                      </p>
+                      <button className="btn btn-primary">Get Started</button>
+                  </div>
+              </div>
+          </div>
       </>
   )
 }
