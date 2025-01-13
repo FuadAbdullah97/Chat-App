18f035422d7d334cca043c29952d126b64bceef9
FuadAbdullah97 <ifuadabdullah@gmail.com>
2025-01-13 22:51:01 +0600

    Signup Frontend With Function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
+import {Route, Routes} from 'react-router-dom';
 import './App.css'
-
-// import Login from "./pages/login/Login.jsx";
-// import SignUp from "./pages/signup/SignUp.jsx";
 import Home from "./pages/home/Home.jsx";
+import Login from "./pages/login/Login.jsx";
+import SignUp from "./pages/signup/SignUp.jsx";
+import {Toaster} from "react-hot-toast";
 
 function App() {
 
 
   return (
-
-      <Home/>
+  <div className="p-4 h-screen flex items-center justify-center">
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route  path="/signup" element={ <SignUp /> } />
+    </Routes>
+    <Toaster/>
+  </div>
 
   );
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,51 +1,101 @@
 import GenderCheck from "./GenderCheck.jsx";
+import {useState} from "react";
+import useSignup from "../../hooks/useSignup.js";
 
-const SignUp = () => {
+
+
+const SignUp =  () => {
+
+const [inputs,setInputs] = useState({
+    fullName: "",
+    userName: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+});
+
+const { loading, signup } = useSignup();
+const handleCheckboxChange = (gender) => {
+    setInputs({...inputs,gender});
+}
+
+
+
+const handleSubmit = async (e)=>{
+    e.preventDefault();
+    await signup(inputs)
+}
+
+
+
     return (
-        <div className="flex flex-col justify-center items-center min-w-96 mx-auto">
+        <form className="flex flex-col justify-center items-center min-w-96 mx-auto" onSubmit={handleSubmit}>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h1 className="text-3xl font-semibold text-center">
                         SignUp <span className="text-primary">ChatAPP</span>
                     </h1>
-
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Full Name</span>
                         </label>
-                        <input type="text" placeholder="Enter Fullname" className="input input-bordered"/>
+                        <input
+                            type="text"
+                            placeholder="Enter Fullname"
+                            className="input input-bordered"
+                            value={inputs.fullName}
+                            onChange={(e) => setInputs({...inputs, fullName: e.target.value})}
+                        />
                     </div>
 
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Username</span>
                         </label>
-                        <input type="text" placeholder="Enter username" className="input input-bordered"/>
+                        <input
+                            type="text"
+                            placeholder="Enter username"
+                            className="input input-bordered"
+                            value={inputs.userName}
+                            onChange={(e) => setInputs({...inputs, userName: e.target.value})}
+                        />
                     </div>
 
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="Enter Password" className="input input-bordered"/>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            className="input input-bordered"
+                            value={inputs.password}
+                            onChange={(e) => setInputs({...inputs, password: e.target.value})}
+                        />
                     </div>
 
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Confirm Password</span>
                         </label>
-                        <input type="password" placeholder="Enter Confirm Password" className="input input-bordered"/>
+                        <input
+                            type="password"
+                            placeholder="Enter Confirm Password"
+                            className="input input-bordered"
+                            value={inputs.confirmPassword}
+                            onChange={(e) =>
+                                setInputs({...inputs, confirmPassword: e.target.value})}
+                        />
                     </div>
 
                     <div>
-                        <GenderCheck/>
+                        <GenderCheck onCheckboxChange={handleCheckboxChange} selectedGender = {inputs.gender} />
                     </div>
-
 
                     <div className="form-control">
                         <label className="cursor-pointer label">
                             <span className="label-text">Remember me</span>
-                            <input type="checkbox"  className="checkbox "/>
+                            <input type="checkbox" className="checkbox"/>
                         </label>
                     </div>
 
@@ -54,7 +104,7 @@ const SignUp = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     );
 };
 
